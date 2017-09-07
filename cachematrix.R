835e49f29149b919fea21e2635c0cb56827b5ade
Melanie Martin <30930087+melaamartin@users.noreply.github.com>
2017-09-06 23:09:27 -0400

    Add files via upload
    melaamartin edited file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+##melaamartin Coursera data science R programming week 3 assignment
+## Function to cache the inverse of a matrix
 
-## Write a short comment describing this function
+## 1. Create a matrix object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <-function(A = matrix()){ 
+    s = NULL #inverse function passed to solve
+    set <- function(Y) {
+        A <<- Y ##
+        s <<- NULL #
+    }
+    get <- function() A 
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get, 
+         setsolve = setsolve,
+         getsolve = getsolve)
+    
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of the matrix above. If inverse has already been calculated, cacheSolve will retrieve inverse from cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(A, ...) {
+    s <- A$getsolve()
+    if(!is.null(s)) { 
+        message("getting cached data")
+        return(s)
+    }
+    data <- A$get()
+    s <- solve(data, ...)
+    A$setsolve(s)
+    s
 }
+
+##test (need to specify a square, nonsingular matrix)
+B <- matrix(rnorm(9), nrow = 3, ncol = 3)##random 9 numbers in a 3 x 3 matrix to avoid a non-singular matrix
+C <- makeCacheMatrix(B)
+C$get()               # retrieve the value of A
+C$getsolve()           # retrieve the value of s, which should be NULL, have to run cacheSolve()
+cacheSolve(C)          # should retrun inverse
+C$getsolve()           # now you can call it directly
